597e5218226d3742e6b87c8abf6f7d8f4bdab5f0
burak soykan <buraksoykan222@hotmail.com>
2023-10-31 12:16:51 +0300

    update h and main files

--- push_swap_main.c
+++ push_swap_main.c
@@ -1,13 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   comooooooooaoaoaooan.c                             :+:      :+:    :+:   */
+/*   push_swap_main.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bsoykan <bsoykan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 14:53:08 by bsoykan           #+#    #+#             */
-/*   Updated: 2023/10/29 14:53:15 by bsoykan          ###   ########.fr       */
+/*   Updated: 2023/10/29 18:27:45 by bsoykan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include "push_swap.h"
+
+int main(int argc, char const *argv[])
+{
+	
+	t_stack *a;
+	t_stack *b;
+	
+	a = (t_stack *)malloc(sizeof(t_stack));
+	if(!a || !b)
+		return 0;
+	a->arr = (int *)malloc(sizeof(int) * (argc - 1));
+	return 0;
+}
